26c2e6489738bf7cc6c72e8073d1320d8e3dc629
PointWolfAnj <anojan31@gmail.com>
2022-03-08 17:59:22 +0000

    Repush

--- src/app.js
+++ src/app.js
@@ -1,12 +1,37 @@
-import Header from './components/header'
-
-import initialEmails from './data/emails'
-
-import './styles/app.css'
+import Header from "./components/header"
+import initialEmails from "./data/emails"
+import "./styles/app.css"
+import { useState } from "react"
 
 function App() {
-  // Use initialEmails for state
-  console.log(initialEmails)
+  const [inbox, setInbox] = useState(initialEmails)
+  const [hideReadBtn, setHideReadBtn] = useState(false)
+
+  const toggleRead = (targetEmail) => {
+    setInbox(
+      inbox.map((email) => {
+        if (email === targetEmail) {
+          return { ...email, read: !email.read }
+        }
+        return email
+      })
+    )
+  }
+
+  const toggleStarred = (targetEmail) => {
+    setInbox(
+      inbox.map((email) => {
+        if (email === targetEmail) {
+          return { ...email, starred: !email.starred }
+        }
+        return email
+      })
+    )
+  }
+
+  const unreadEmails = inbox.filter((email) => email.read === false)
+  const starredEmails = inbox.filter((email) => email.starred === true)
+  const readEmails = inbox.filter((email) => email.read === true)
 
   return (
     <div className="app">
@@ -18,14 +43,14 @@ function App() {
             // onClick={() => {}}
           >
             <span className="label">Inbox</span>
-            <span className="count">?</span>
+            <span className="count">{unreadEmails.length}</span>
           </li>
           <li
             className="item"
             // onClick={() => {}}
           >
             <span className="label">Starred</span>
-            <span className="count">?</span>
+            <span className="count">{starredEmails.length}</span>
           </li>
 
           <li className="item toggle">
@@ -33,13 +58,40 @@ function App() {
             <input
               id="hide-read"
               type="checkbox"
-              checked={false}
-              // onChange={() => {}}
+              onChange={() => setHideReadBtn(true)}
+              checked={hideReadBtn}
             />
           </li>
         </ul>
       </nav>
-      <main className="emails">{/* Render a list of emails here */}</main>
+      <main className="emails">
+        <ul>
+          {inbox.map((email) => {
+            return (
+              <li className="email">
+                <div className="select">
+                  <input
+                    className="select-checkbox"
+                    type="checkbox"
+                    onChange={() => toggleRead(email)}
+                    checked={email.read}
+                  />
+                </div>
+                <div className="star">
+                  <input
+                    className="star-checkbox"
+                    type="checkbox"
+                    onChange={() => toggleStarred(email)}
+                    checked={email.starred}
+                  />
+                </div>
+                <div className="sender">{email.sender}</div>
+                <div className="title">{email.title}</div>
+              </li>
+            )
+          })}
+        </ul>
+      </main>
     </div>
   )
 }
